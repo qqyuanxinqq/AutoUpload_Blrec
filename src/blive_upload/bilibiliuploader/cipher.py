45358ff7fe3f36d8f9c81439e7e4a24e47b6a9aa
XY <qqyuanxinqq@gmail.com>
2024-09-09 22:10:23 -0400

    FIxed the av-bv converision

--- src/blive_upload/bilibiliuploader/cipher.py
+++ src/blive_upload/bilibiliuploader/cipher.py
@@ -66,33 +66,35 @@ def encrypt_login_password(password, hash, pubkey):
         rsa.PublicKey.load_pkcs1_openssl_pem(pubkey.encode()),
     ))
 
+XOR_CODE = 23442827791579
+MASK_CODE = 2251799813685247
+MAX_AID = 1 << 51
+ALPHABET = "FcwAPNKTMug3GV5Lj7EJnHpWsx4tb8haYeviqBz6rkCy12mUSDQX9RdoZf"
+ENCODE_MAP = 8, 7, 0, 5, 1, 3, 2, 4, 6
+DECODE_MAP = tuple(reversed(ENCODE_MAP))
 
-def av2bv(av: int):
-    table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
-    tr = {}
-    for i in range(58):
-        tr[table[i]] = i
-    s = [11, 10, 3, 8, 4, 6]
-    xor = 177451812
-    add = 8728348608
+BASE = len(ALPHABET)
+PREFIX = "BV1"
+PREFIX_LEN = len(PREFIX)
+CODE_LEN = len(ENCODE_MAP)
 
-    av = (av ^ xor) + add
-    r = list('BV1  4 1 7  ')
-    for i in range(6):
-        r[s[i]] = table[av // 58 ** i % 58]
-    return ''.join(r)
+def av2bv(aid: int) -> str:
+    bvid = [""] * 9
+    tmp = (MAX_AID | aid) ^ XOR_CODE
+    for i in range(CODE_LEN):
+        bvid[ENCODE_MAP[i]] = ALPHABET[tmp % BASE]
+        tmp //= BASE
+    return PREFIX + "".join(bvid)
 
+def bv2av(bvid: str) -> int:
+    assert bvid[:3] == PREFIX
 
-def bv2av(bv: str):
-    table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
-    tr = {}
-    for i in range(58):
-        tr[table[i]] = i
-    s = [11, 10, 3, 8, 4, 6]
-    xor = 177451812
-    add = 8728348608
+    bvid = bvid[3:]
+    tmp = 0
+    for i in range(CODE_LEN):
+        idx = ALPHABET.index(bvid[DECODE_MAP[i]])
+        tmp = tmp * BASE + idx
+    return (tmp & MASK_CODE) ^ XOR_CODE
 
-    r = 0
-    for i in range(6):
-        r += tr[bv[s[i]]] * 58 ** i
-    return (r - add) ^ xor
+assert av2bv(111298867365120) == "BV1L9Uoa9EUx"
+assert bv2av("BV1L9Uoa9EUx") == 111298867365120
